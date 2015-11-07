ff4bd0e6423f350471facbd81839d16ad410642c
JeffBobbo <james.delph@hotmail.co.uk>
2015-11-07 00:08:51 +0000

    RandomAlgorithm improvements

--- RandomAlgorithm.java
+++ RandomAlgorithm.java
@@ -18,14 +18,7 @@ public class RandomAlgorithm
     Random random = new Random();
     int digits = 0;
     while (digits % 1111 == 0)
-    {
-      digits = 0;
-      for (int i = 0; i < 4; ++i)
-      {
-        final int digit = random.nextInt(10);
-        digits += digit * Math.pow(10, i);
-      }
-    }
+      digits = random.nextInt(9998) + 1; // go 0..9997 then add 1 to get 1..9998
     return digits;
   }
 
@@ -39,13 +32,18 @@ public class RandomAlgorithm
     int lastNumber = -1;
     for (int i = 0; i < 10; ++i)
     {
-      int d1 = generateDigits();
-      int d2 = generateDigits();
+      // generate two sets of digits
+      final int d1 = generateDigits();
+      final int d2 = generateDigits();
 
+      // and now the two sets of numbers
       final int bigger = d1 > d2 ? d1 : d2;
       final int smaller = d1 > d2 ? d2 : d1;
-
+      // and work out our value
       final int number = bigger - smaller;
+      // this could be a bit shorter: final int number = Math.abs(d1 - d2);
+      // but the assignment stipulates two variables, 'bigger' and 'smaller'.
+
       System.out.printf("%04d-%04d=%04d%n", bigger, smaller, number);
       if (number == lastNumber)
         break;
